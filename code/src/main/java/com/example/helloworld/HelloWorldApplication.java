be9bf7e6c7c6151837797fd8df627153c66394a5
Luram Archanjo <luram.archanjo@sensedia.com>
2019-06-04 19:26:12 -0300

    Draft

--- code/src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ code/src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -1,32 +1,14 @@
 package com.example.helloworld;
 
-import static spark.Spark.*;
+import static spark.Spark.get;
+import static spark.Spark.port;
 
 public class HelloWorldApplication {
-	
-	static String  message;
 
-	
-	public static void main(String args[]) {
-		
-	/*
-     * Please Note: 
-     * Default port in SparkJava is 4567
-     * If you wish to use port of your choice, 
-     * uncomment the following code and set the
-     * required port
-     */
-
-		//private static final String PORT = "8080";
-        //port(Integer.valueOf(System.getenv("PORT")));
-			
-		if( System.getenv("TARGET") == null) 
-			message = "World";
-		else
-			message = System.getenv("TARGET");
-		
-		get("/", (req,res) -> "Hello " + message);
-	}
-	
+  public static void main(String args[]) {
+    port(Integer.valueOf(System.getenv("PORT")));
+    final String message = (System.getenv("TARGET") != null) ? System.getenv("TARGET") : "World";
+    get("/", (req, res) -> message);
+  }
 
 }
