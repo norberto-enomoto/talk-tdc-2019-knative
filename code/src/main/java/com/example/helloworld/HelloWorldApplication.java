53ba99e754563423910322ff6ebc989f4073ab20
Luram Archanjo <luram.archanjo@sensedia.com>
2019-06-04 19:28:29 -0300

    Draft

--- code/src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ code/src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -7,7 +7,9 @@ public class HelloWorldApplication {
 
   public static void main(String args[]) {
     port(Integer.valueOf(System.getenv("PORT")));
-    final String message = (System.getenv("TARGET") != null) ? System.getenv("TARGET") : "World";
+    final String defaultMessage = "Default message use the environment TARGET to set a new one";
+    final String targetEnvironment = System.getenv("TARGET");
+    final String message = (targetEnvironment != null) ? targetEnvironment : defaultMessage;
     get("/", (req, res) -> message);
   }
 
