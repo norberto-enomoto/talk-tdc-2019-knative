624aa246307f4f27e44d5cf766905e8ba054fdd8
Luram Archanjo <luram.archanjo@sensedia.com>
2019-06-09 11:18:13 -0300

    Installing Camel K

---
+++ setup/4_install_kamel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Creating GKE Service account"
+gcloud iam service-accounts create camel-k-builder --display-name "Camel-K Builder"
+gcloud projects add-iam-policy-binding $PROJECT_ID --member serviceAccount:camel-k-builder@$PROJECT_ID.iam.gserviceaccount.com --role roles/storage.admin
+gcloud iam service-accounts keys create kaniko-secret.json --iam-account camel-k-builder@$PROJECT_ID.iam.gserviceaccount.com
+
+echo "Creating K8S Service account"
+kubectl create secret generic kaniko-secret --from-file=kaniko-secret.json
+
+echo "Removing kaniko-secret.json file"
+rm kaniko-secret.json
+
+echo "Installing Camel-K"
+kamel install --registry gcr.io --organization $PROJECT_ID --push-secret kaniko-secret
